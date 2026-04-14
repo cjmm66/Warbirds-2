0e5f9db407c8333b2a88bf7314d1a87fe42108b0
Cristian Jose Meza Montes <cj200309@gmail.com>
2026-04-14 18:59:40 -0400

    Main Menu Completed

---
+++ Assets/Scripts/GameMainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMainMenu : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName;
+    public void Play()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
